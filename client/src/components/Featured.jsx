3f2dfbd09fd8be7a388e9a30a807e649eee7f993
Yash Gaur <yashgaur2525@gmail.com>
2024-09-12 00:57:03 +0530

    search bar ui changes

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -4,12 +4,12 @@ const Featured = () => {
   return (
     <section className="bg-gray-100 py-16">
       <div className="container mx-auto px-6">
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="md:w-1/2">
-            <img src={FeaturedImage} alt="Featured" className="rounded-3xl shadow-lg" />
-          </div>
-          <div className="md:w-1/2 mt-8 md:mt-0 md:pl-16">
-            <h2 className="text-4xl font-bold text-gray-800 mb-4">Breaking Into Product Design</h2>
+        <div className=" md:flex-row items-center">
+            <img src={FeaturedImage} alt="Featured" className="rounded-3xl  shadow-lg" />
+          {/* <div className="md:w-1/2">
+          </div> */}
+          <div className="md:w-1/2 -mt-28 z-10 bg-white md:-mt-60 md:pl-16">
+            <h2 className="text-4xl font-bold text-white mb-4 lg:text-4xl sm:text-xl">Breaking Into Product Design</h2>
             <p className="text-gray-600 text-lg mb-4">
               Advice from Untitled Founder, Frankie. Learn how to get into product design and grow in the industry.
             </p>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -103,8 +103,8 @@ const Header = () => {
 
         {/* Pop-up Search Bar */}
         {isSearchOpen && (
-          <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center  z-50">
-            <div className="bg-white  p-6 rounded-lg w-full max-w-lg shadow-lg">
+          <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-start  justify-center py-32 ">
+            <div className="bg-white  p-6 rounded-lg w-full mx-5 h-42 max-w-lg shadow-lg">
               <div className="flex justify-between items-center mb-4">
                 <h2 className="text-xl font-semibold">Search</h2>
                 <button onClick={toggleSearch} className="focus:outline-none">
